deb0465bc760053ff96abfe89525375b3206a293
Nathan Johnson <nathanjohnson320@gmail.com>
2022-03-13 10:38:06 -0400

    moving everything around

--- lib/charon.ex
+++ lib/charon.ex
@@ -29,7 +29,13 @@ defmodule Charon do
   end
   ```
 
-  This will wrap your create module in a function that calls Validators.NewThing.Create.validate/2
+  This will wrap your create module in a function that calls Validators.NewThing.Create.Request.validate/2.
+  If you need to use a different request module name for some reason you can pass it directly
+
+  ```
+  @charon [request: Validators.NewThing.SomeSpecificModule, response: Response.SomeSpecificModule]
+  def create(conn, params) do
+  ```
 
   Your validator can be anything that returns an ecto changeset result or really anything that returns
   `%{valid?: boolean, errors: []}`
@@ -108,18 +114,216 @@ defmodule Charon do
     * The http status code to return when the validation fails. Defaults to 422
     * example: `config :charon, error_code: 400`
   """
+
+  require Charon.Request
+
   defmacro __using__(_args) do
     quote do
       require Charon
-      require Charon.Request
+
       # Ensure list of charon functions is accumulated at compile time
       Module.register_attribute(__MODULE__, :charon_functions, accumulate: true)
       # Do not accumulate the last function checked
       Module.register_attribute(__MODULE__, :charon_last, accumulate: false)
       # Keep track of request status codes
       Module.register_attribute(__MODULE__, :status_code, accumulate: true)
-      @before_compile Charon.Request
-      @on_definition Charon.Request
+      @before_compile Charon
+      @on_definition Charon
+    end
+  end
+
+  require Logger
+
+  def __on_definition__(_env, _access, _name, _args, _guards, nil), do: nil
+  def __on_definition__(_env, _access, _name, _args, _guards, []), do: nil
+
+  # When functions are defined that have @charon transform add them to the :charon_functions accumulator
+  def __on_definition__(%{module: module}, access, name, args, guards, do: body) do
+    if info = charon_function?(module, name, length(args)) do
+      Module.put_attribute(module, :charon_functions, %{
+        module: module,
+        access: access,
+        function: name,
+        args: args,
+        guards: guards,
+        body: body,
+        charon_info: info
+      })
+
+      Module.put_attribute(module, :charon_last, {name, length(args)})
+      Module.delete_attribute(module, :charon)
+    end
+  end
+
+  # Take no action if there are other function-level clauses
+  def __on_definition__(%{module: module}, _access, name, args, _guards, clauses) do
+    if charon_function?(module, name, length(args)) do
+      found =
+        clauses
+        |> Keyword.drop([:do])
+        |> Keyword.keys()
+        |> Enum.map(&"`#{&1}`")
+        |> Enum.join(", ")
+
+      Logger.warn(
+        "[Charon] Unable to wrap `#{inspect(module)}.#{name}/#{length(args)}` " <>
+          "due to additional function-level clauses: #{found} -- please remove @charon"
+      )
+
+      Module.delete_attribute(module, :charon)
+    end
+  end
+
+  # Before compile take all the charon functions and make them overridable
+  defmacro __before_compile__(%{module: module}) do
+    Module.delete_attribute(module, :charon_last)
+    Module.make_overridable(module, function_specs(module))
+
+    quote do
+      (unquote_splicing(function_definitions(module)))
+    end
+  end
+
+  # Return list of functions that we need to mark as overridable
+  def function_specs(module),
+    do:
+      module
+      |> Module.get_attribute(:charon_functions)
+      |> Enum.map(fn %{function: function, args: args} -> {function, length(args)} end)
+      |> Enum.uniq()
+
+  # Function to loop over :charon_function tagged functions and wrap them with the auto validate
+  def function_definitions(module),
+    do:
+      module
+      |> Module.get_attribute(:charon_functions)
+      |> Enum.map(&wrapped_function_definition/1)
+      |> Enum.reverse()
+
+  # Clause for def with no guards
+  def wrapped_function_definition(%{
+        module: module,
+        access: :def,
+        function: function,
+        args: args,
+        body: body,
+        guards: [],
+        charon_info: charon_info
+      }) do
+    quote do
+      def unquote(function)(unquote_splicing(args)) do
+        unquote(wrapped_function_body(body, module, function, args, charon_info))
+      end
+    end
+  end
+
+  # Clause for def with guards
+  def wrapped_function_definition(%{
+        module: module,
+        access: :def,
+        function: function,
+        args: args,
+        body: body,
+        guards: guards,
+        charon_info: charon_info
+      }) do
+    quote do
+      def unquote(function)(unquote_splicing(args))
+          when unquote_splicing(guards) do
+        unquote(wrapped_function_body(body, module, function, args, charon_info))
+      end
+    end
+  end
+
+  # Clause for defp with no guards
+  def wrapped_function_definition(%{
+        module: module,
+        access: :defp,
+        function: function,
+        args: args,
+        body: body,
+        guards: [],
+        charon_info: charon_info
+      }) do
+    quote do
+      defp unquote(function)(unquote_splicing(args)) do
+        unquote(wrapped_function_body(body, module, function, args, charon_info))
+      end
+    end
+  end
+
+  # Clause for defp with guards
+  def wrapped_function_definition(%{
+        module: module,
+        access: :defp,
+        function: function,
+        args: args,
+        body: body,
+        guards: guards,
+        charon_info: charon_info
+      }) do
+    quote do
+      defp unquote(function)(unquote_splicing(args))
+           when unquote_splicing(guards) do
+        unquote(wrapped_function_body(body, module, function, args, charon_info))
+      end
+    end
+  end
+
+  @doc ~S"""
+  Provided a given controller function and a validator module this function wraps the function below it
+  with `RequestModule.validate(conn, params)` and renders the errors based on charon: :error_view and
+  status code from charon: :error_code
+
+  For example if you have
+
+  ```
+  @charon Request.Create
+  def create(conn, params) do
+    <internals>
+  end
+  ```
+
+  that is roughly equivalent to
+
+  ```
+  def create(conn, params) do
+    case apply(Request.Create.Request, :validate, [conn, params]) do
+      %{valid? true} -> <internals>
+      %{errors: _errors} = changeset ->
+        error_view = Application.get_env(:charon, :error_view)
+        error_code = Application.get_env(:charon, :error_code, 422)
+        conn |> put_status(error_code) |> put_view(error_view) |> render("error.json", changeset: changeset)
+    end
+  end
+  ```
+  """
+  def wrapped_function_body(body, module, function, args, charon_module)
+      when is_atom(charon_module) do
+    request = Module.concat(charon_module, Request)
+    response = Module.concat(charon_module, Response)
+    wrapped_function_body(body, module, function, args, request: request, response: response)
+  end
+
+  def wrapped_function_body(body, _module, _function, [conn | _rest] = args,
+        request: request,
+        response: _response
+      ) do
+    Charon.Request.validate(%{module: request, conn: conn}, args, body)
+  end
+
+  def charon_function?(module, name, arity),
+    do:
+      charon_function?(:via_annotation, module) ||
+        charon_function?(:via_multiple_heads, module, name, arity)
+
+  def charon_function?(:via_annotation, module),
+    do: Module.get_attribute(module, :charon)
+
+  def charon_function?(:via_multiple_heads, module, name, arity) do
+    case Module.get_attribute(module, :charon_last) do
+      {^name, ^arity, info} -> info
+      _ -> false
     end
   end
 end
--- lib/request.ex
+++ lib/request.ex
@@ -1,189 +1,29 @@
 defmodule Charon.Request do
   @moduledoc ~S"""
-  Compile time macro that adds support for the `@charon`
-
-  You shouldn't have to interact with this module directly it is auto included with `use Charon`
+  Module that adds some simple logic to keep track of request information.
   """
 
-  require Logger
-
-  def __on_definition__(_env, _access, _name, _args, _guards, nil), do: nil
-  def __on_definition__(_env, _access, _name, _args, _guards, []), do: nil
-
-  # When functions are defined that have @charon transform add them to the :charon_functions accumulator
-  def __on_definition__(%{module: module}, access, name, args, guards, do: body) do
-    if info = charon_function?(module, name, length(args)) do
-      Module.put_attribute(module, :charon_functions, %{
-        module: module,
-        access: access,
-        function: name,
-        args: args,
-        guards: guards,
-        body: body,
-        charon_info: info
-      })
-
-      Module.put_attribute(module, :charon_last, {name, length(args)})
-      Module.delete_attribute(module, :charon)
-    end
-  end
-
-  # Take no action if there are other function-level clauses
-  def __on_definition__(%{module: module}, _access, name, args, _guards, clauses) do
-    if charon_function?(module, name, length(args)) do
-      found =
-        clauses
-        |> Keyword.drop([:do])
-        |> Keyword.keys()
-        |> Enum.map(&"`#{&1}`")
-        |> Enum.join(", ")
-
-      Logger.warn(
-        "[Charon] Unable to wrap `#{inspect(module)}.#{name}/#{length(args)}` " <>
-          "due to additional function-level clauses: #{found} -- please remove @charon"
-      )
-
-      Module.delete_attribute(module, :charon)
-    end
-  end
-
-  # Before compile take all the charon functions and make them overridable
-  defmacro __before_compile__(%{module: module}) do
-    Module.delete_attribute(module, :charon_last)
-    Module.make_overridable(module, function_specs(module))
-
+  defmacro __using__(_args) do
     quote do
-      (unquote_splicing(function_definitions(module)))
+      use Charon.Schema
+
+      # Keep track of request status codes
+      Module.register_attribute(__MODULE__, :status_code, persist: true, accumulate: false)
     end
   end
 
-  # Return list of functions that we need to mark as overridable
-  def function_specs(module),
-    do:
-      module
-      |> Module.get_attribute(:charon_functions)
-      |> Enum.map(fn %{function: function, args: args} -> {function, length(args)} end)
-      |> Enum.uniq()
-
-  # Function to loop over :charon_function tagged functions and wrap them with the auto validate
-  def function_definitions(module),
-    do:
-      module
-      |> Module.get_attribute(:charon_functions)
-      |> Enum.map(&wrapped_function_definition/1)
-      |> Enum.reverse()
-
-  # Clause for def with no guards
-  def wrapped_function_definition(%{
-        module: module,
-        access: :def,
-        function: function,
-        args: args,
-        body: body,
-        guards: [],
-        charon_info: charon_info
-      }) do
-    quote do
-      def unquote(function)(unquote_splicing(args)) do
-        unquote(wrapped_function_body(body, module, function, args, charon_info))
-      end
-    end
-  end
-
-  # Clause for def with guards
-  def wrapped_function_definition(%{
-        module: module,
-        access: :def,
-        function: function,
-        args: args,
-        body: body,
-        guards: guards,
-        charon_info: charon_info
-      }) do
-    quote do
-      def unquote(function)(unquote_splicing(args))
-          when unquote_splicing(guards) do
-        unquote(wrapped_function_body(body, module, function, args, charon_info))
-      end
-    end
-  end
-
-  # Clause for defp with no guards
-  def wrapped_function_definition(%{
-        module: module,
-        access: :defp,
-        function: function,
-        args: args,
-        body: body,
-        guards: [],
-        charon_info: charon_info
-      }) do
-    quote do
-      defp unquote(function)(unquote_splicing(args)) do
-        unquote(wrapped_function_body(body, module, function, args, charon_info))
-      end
-    end
-  end
-
-  # Clause for defp with guards
-  def wrapped_function_definition(%{
-        module: module,
-        access: :defp,
-        function: function,
-        args: args,
-        body: body,
-        guards: guards,
-        charon_info: charon_info
-      }) do
-    quote do
-      defp unquote(function)(unquote_splicing(args))
-           when unquote_splicing(guards) do
-        unquote(wrapped_function_body(body, module, function, args, charon_info))
-      end
-    end
-  end
-
-  @doc ~S"""
-  Provided a given controller function and a validator module this function wraps the function below it
-  with `RequestModule.validate(conn, params)` and renders the errors based on charon: :error_view and
-  status code from charon: :error_code
-
-  For example if you have
-
-  ```
-  @charon Request.Create
-  def create(conn, params) do
-    <internals>
-  end
-  ```
-
-  that is roughly equivalent to
-
-  ```
-  def create(conn, params) do
-    case apply(Request.Create, :validate, [conn, params]) do
-      %{valid? true} -> <internals>
-      %{errors: _errors} = changeset ->
-        error_view = Application.get_env(:charon, :error_view)
-        error_code = Application.get_env(:charon, :error_code, 422)
-        conn |> put_status(error_code) |> put_view(error_view) |> render("error.json", changeset: changeset)
-    end
-  end
-  ```
-  """
-  def wrapped_function_body(body, _module, _function, [conn | _rest] = args, validator) do
-    safe_args = Enum.filter(args, &unused_args/1)
-    request_module = Module.concat(validator, Request)
+  def validate(%{module: module, conn: conn}, function_args, body) do
+    function_args = Enum.filter(function_args, &unused_args/1)
 
     status_code =
       :attributes
-      |> request_module.__info__()
+      |> module.__info__()
       |> Keyword.get(:status_code)
       |> List.first()
 
     quote do
-      case apply(unquote(request_module), :validate, [
-             unquote_splicing(safe_args)
+      case apply(unquote(module), :validate, [
+             unquote_splicing(function_args)
            ]) do
         %{valid?: true} ->
           unquote(body)
@@ -206,30 +46,6 @@ defmodule Charon.Request do
     case arg do
       <<"_", _rest::binary>> -> false
       _ -> true
-    end
-  end
-
-  def charon_function?(module, name, arity),
-    do:
-      charon_function?(:via_annotation, module) ||
-        charon_function?(:via_multiple_heads, module, name, arity)
-
-  def charon_function?(:via_annotation, module),
-    do: Module.get_attribute(module, :charon)
-
-  def charon_function?(:via_multiple_heads, module, name, arity) do
-    case Module.get_attribute(module, :charon_last) do
-      {^name, ^arity, info} -> info
-      _ -> false
-    end
-  end
-
-  defmacro __using__(_args) do
-    quote do
-      use Charon.Schema
-
-      # Keep track of request status codes
-      Module.register_attribute(__MODULE__, :status_code, persist: true, accumulate: false)
     end
   end
 end
